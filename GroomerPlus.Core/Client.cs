cbbe7eace140e0e89c517c77cff4600ed842e722
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-11-16 11:19:41 -0500

    Added `Client` Entity
    Added the `Client` entity class to represent the owners of the pets that
    a groomer is grooming.

---
+++ GroomerPlus.Core/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GroomerPlus.Core
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
